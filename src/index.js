891ebef3ec87005e11da3e03e5d16e7a8aae9344
antonio63963 <shotvideo25@gmail.com>
2024-07-13 18:46:15 +0300

    mobile layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './common/styles/index.css';
+
+const sandwich = document.querySelector('.page-content__sandwich');
+const sidebar = document.querySelector('.sidebar');
+const closeSidebar = document.querySelector('.sidebar__close');
+
+function onOpenSidebar(e) {
+  sidebar.classList.add('opened');
+}
+
+function onCloseSidebar(e) {
+  sidebar.classList.remove('opened');
+}
+
+sandwich.addEventListener('click', onOpenSidebar);
+closeSidebar.addEventListener('click', onCloseSidebar);
